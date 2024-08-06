e8a921ab0e5753f1b3c595039cbe1a1c3b678594
zienex <szymon.zienkiewicz5@gmail.com>
2024-08-06 21:43:57 +0200

    Finished the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,91 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Text(
+            'DiceDuel',
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Roboto'),
+          ),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int rightDiceImageNumber = 1;
+  int leftDiceImageNumber = 1;
+
+  void changeDiceRandomly() {
+    setState(() {
+      rightDiceImageNumber = Random().nextInt(6) + 1;
+      leftDiceImageNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                  onTap: () {
+                    changeDiceRandomly();
+                  },
+                  child: Image.asset('images/dice$leftDiceImageNumber.png')),
+              const Text(
+                'Player one',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Roboto'),
+              )
+            ],
+          )),
+          Expanded(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                  onTap: () {
+                    changeDiceRandomly();
+                  },
+                  child: Image.asset('images/dice$rightDiceImageNumber.png')),
+              const Text(
+                'Player two',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Roboto'),
+              )
+            ],
+          )),
+        ],
+      ),
+    );
+  }
+}
